1ccbbcaa2c227215eeb399726cbb6bdaf218c469
Rumazor <th3rum2084@gmail.com>
2022-05-14 21:57:29 -0500

    aside menu listo

---
+++ app.js
@@ -0,0 +1,11 @@
+const navToggle = document.querySelector(".nav-toggle");
+const closeAsideBtn = document.querySelector("#close-aside");
+const asideMenu = document.querySelector(".aside-menu");
+
+navToggle.addEventListener("click", () => {
+  asideMenu.classList.add("show-aside");
+});
+
+closeAsideBtn.addEventListener("click", () => {
+  asideMenu.classList.remove("show-aside");
+});
